d7120d1bee810c03a8e9ca63c59e078c23c55d24
miguel-nascimento <miguelgomes13@live.com>
2021-06-21 13:35:04 -0300

    feat: dark default theme

---
+++ src/styles/dark.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from 'styled-components';
+
+const dark: DefaultTheme = {
+  title: 'dark',
+  color: {
+    text: {
+      white: "#FFFFFF",
+      yellow: "#FEE147",
+      black: "#000000",
+    }
+  },
+};
+
+export default dark;
