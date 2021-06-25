7ff3065fd2e72ae3fdc82560676cc0d3ac7655b8
CarmichaelF <carmichaelferreira06@gmail.com>
2021-06-25 15:07:04 -0300

    Adding the HowItWorks and GetInTouch components to the project

---
+++ src/components/Sections/HowItWorks/styles.ts
@@ -0,0 +1,131 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  padding: 125px 0;
+  background: radial-gradient(
+      50% 50% at 50% 50%,
+      #000000 0%,
+      rgba(0, 0, 0, 0.42) 100%
+    ),
+    #332d2b;
+  width: 100%;
+  max-width: 1440px;
+  margin: 0 auto;
+  position: relative;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    padding: 0;
+  }
+`;
+
+export const Wrapper = styled.section`
+  max-width: 1075px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const LeftContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  z-index: 10;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 48px;
+  color: ${({ theme }) => theme.text.color.white};
+  margin-bottom: 55px;
+  line-height: 54.5px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    text-align: center;
+    font-size: 24px;
+    line-height: 30px;
+    margin: 30px 0;
+  }
+
+  > strong {
+    background: ${({ theme }) => theme.text.color.gradient};
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+  }
+`;
+
+export const Steps = styled.div`
+  max-width: 635px;
+  transition: 0.5s;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    max-width: 100%;
+  }
+`;
+
+interface StepProps {
+  color: string;
+  badgeBackground: string;
+  backgroundColor: string;
+}
+
+export const Step = styled.div<StepProps>`
+  display: flex;
+  align-items: center;
+  padding: 48px 80px;
+  background: ${({ badgeBackground }) => badgeBackground};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    padding: 30px 50px;
+  }
+
+  > span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-width: 95px;
+    min-height: 95px;
+    font-weight: bold;
+    font-size: 36px;
+    color: ${({ theme }) => theme.text.color.white};
+    background-color: ${({ backgroundColor }) => backgroundColor};
+    border-radius: 50%;
+    margin-right: 80px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+      min-width: 45px;
+      min-height: 45px;
+      font-size: 14px;
+      margin-right: 25px;
+    }
+  }
+
+  > div {
+    color: ${({ color }) => color};
+    > h2 {
+      margin-bottom: 8px;
+      font-weight: 800;
+      font-size: 14px;
+      line-height: 18px;
+      text-transform: uppercase;
+    }
+    > p {
+      font-size: 14px;
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+        font-size: 12px;
+      }
+    }
+  }
+`;
+
+export const RightContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translate(0, -50%);
+  z-index: 5;
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,8 @@
-import React, {
-  useEffect
-} from "react";
-import {
-  useLocation,
-} from '@reach/router';
-import {
-  navigate,
-} from 'gatsby';
+import React, { useEffect } from "react";
+import { useLocation } from "@reach/router";
+import { navigate } from "gatsby";
 
-import {
-  locales
-} from 'src/constants';
+import { locales } from "src/constants";
 import First from "src/components/Sections/First";
 import GlobalStyle from "src/styles/globalStyle";
 import { ThemeProvider } from "styled-components";
@@ -18,21 +10,27 @@ import Layout from "../components/Layout";
 import dark from "../styles/dark";
 import messages from "src/i18n/messages";
 
+import HowItWorks from "../components/Sections/HowItWorks";
+import GetInTouch from "../components/Sections/GetInTouch";
+
 const IndexPage = () => {
   const location = useLocation();
-  const i18n = navigator.language === locales.pt ? messages.pt : messages.en;
-
+  const {
+    home: { firstSection, thirdSection, getInTouch },
+  } = navigator.language === locales.pt ? messages.pt : messages.en;
   return (
     <ThemeProvider theme={dark}>
       <GlobalStyle />
       <Layout>
         <First
-          title={i18n.home.firstSection.title}
-          description={i18n.home.firstSection.description}
-          buttonText={i18n.home.firstSection.buttonText}
+          title={firstSection.title}
+          description={firstSection.description}
+          buttonText={firstSection.buttonText}
         />
+        <HowItWorks {...thirdSection} />
+        <GetInTouch {...getInTouch} />
       </Layout>
     </ThemeProvider>
   );
-}
+};
 export default IndexPage;
