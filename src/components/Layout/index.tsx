495a365bf0a3c7388243e842faececbd4ce10c48
vitorregisr <vitorregisrr@gmail.com>
2021-07-14 18:29:22 -0300

    Add react-helmet

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "../common/Footer";
 import { Container } from "./styles";
+import { Helmet } from "react-helmet";
 
 import "@fontsource/open-sans/300.css";
 import "@fontsource/open-sans/400.css";
@@ -10,6 +11,13 @@ import "@fontsource/manrope/800.css";
 
 const Layout: React.FC = ({ children }) => (
   <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>
+        Foton Bank • International transfers for individuals and companies of
+        all sizes.
+      </title>
+    </Helmet>
     <Container>{children}</Container>
     <Footer />
   </>
